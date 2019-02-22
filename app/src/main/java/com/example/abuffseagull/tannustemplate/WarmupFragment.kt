bf9b51acfc4f081dfe06974d38d1039cccdc0845
ABuffSeagull <reecevanatta@gmail.com>
2019-02-22 17:24:23 -0500

    Refactor the Warmup Layout
    Using a TableLayout, instead of making crazy constraint layout bullshit

--- app/src/main/java/com/example/abuffseagull/tannustemplate/WarmupFragment.kt
+++ app/src/main/java/com/example/abuffseagull/tannustemplate/WarmupFragment.kt
@@ -1,11 +1,11 @@
 package com.example.abuffseagull.tannustemplate
 
 import android.os.Bundle
-import android.support.constraint.ConstraintSet
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableRow
 import android.widget.TextView
 import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_warmup.*
@@ -20,59 +20,35 @@ class WarmupFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         // NOTE: everything is * 10 so that it's all ints
-        val currentWeight = 2250
+        val currentWeight = 2150
         warmupText.text = getString(R.string.warmup_weight, currentWeight / 10)
         var leftoverWeight = (currentWeight - 450) / 2
-        var lastId = 0
 
-        val constraintSet = ConstraintSet()
-        constraintSet.clone(warmupLayout)
         for (weight in arrayOf(450, 250, 100, 50, 25)) {
             val count = leftoverWeight / weight
-            if (count != 0) {
-                val countView = TextView(context)
-                countView.text = count.toString()
-                countView.id = View.generateViewId()
-                countView.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
-                countView.textSize = 24f
-                warmupLayout.addView(countView)
-                constraintSet.constrainHeight(countView.id, ConstraintSet.WRAP_CONTENT)
-                constraintSet.constrainWidth(countView.id, ConstraintSet.WRAP_CONTENT)
-                constraintSet.connect(
-                    countView.id,
-                    ConstraintSet.TOP,
-                    if (lastId != 0) lastId else countTitle.id,
-                    ConstraintSet.BOTTOM,
-                    8
-                )
+            if (count == 0) continue
+            val row = TableRow(context)
 
-                val weightView = TextView(context)
-                weightView.text = if (weight == 25) "2.5" else (weight / 10).toString()
-                weightView.id = View.generateViewId()
-                weightView.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
-                weightView.textSize = 24f
-                warmupLayout.addView(weightView)
-                constraintSet.constrainHeight(weightView.id, ConstraintSet.WRAP_CONTENT)
-                constraintSet.constrainWidth(weightView.id, ConstraintSet.WRAP_CONTENT)
-                constraintSet.centerVertically(weightView.id, countView.id)
+            val countView = TextView(context)
+            countView.text = count.toString()
+            countView.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+            countView.textSize = 24f
+            row.addView(
+                countView,
+                TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f)
+            )
 
-                constraintSet.createHorizontalChain(
-                    ConstraintSet.PARENT_ID,
-                    ConstraintSet.LEFT,
-                    ConstraintSet.PARENT_ID,
-                    ConstraintSet.RIGHT,
-                    intArrayOf(countView.id, weightView.id),
-                    floatArrayOf(1f, 1f),
-                    ConstraintSet.CHAIN_SPREAD
-                )
-
-                constraintSet.centerHorizontally(countView.id, if (lastId != 0) lastId else countTitle.id)
-                constraintSet.constrainHeight(countView.id, ConstraintSet.WRAP_CONTENT)
-                lastId = countView.id
-            }
+            val weightView = TextView(context)
+            weightView.text = if (weight == 25) "2.5" else (weight / 10).toString()
+            weightView.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+            weightView.textSize = 24f
+            row.addView(
+                weightView,
+                TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f)
+            )
+            plateLayout.addView(row)
             leftoverWeight -= count * weight
         }
-        constraintSet.applyTo(warmupLayout)
 
         nextStage.setOnClickListener {
             it.findNavController().navigate(R.id.action_warmupFragment_to_setFragment)
